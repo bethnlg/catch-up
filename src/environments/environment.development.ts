96015937d4589d66871ef71c2a859f30705e1387
Beth Oneglio <106390542+bethnlg@users.noreply.github.com>
2024-09-05 19:51:40 -0500

    chore: configured environment variables.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+// Environment: development
+// This is the development environment configuration file.
+// It contains the configuration settings for the development environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: false,
+  newsApiKey: '0d5b87d6eed74a768b7f2f7a3ca1bafb'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// Environment: production
+// This is the production environment configuration file.
+// It contains the configuration settings for the production environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: true,
+  newsApiKey: '0d5b87d6eed74a768b7f2f7a3ca1bafb'
+};
